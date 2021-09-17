1b645a3f406459f6206663588722bf3b19ddce7a
christian <jc.yunguridata@gmail.com>
2021-09-17 12:28:03 -0500

    update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,11 +1,19 @@
 const env = require('./env.js');
  
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(env.database, env.username, env.password, {
+const sequelize = new Sequelize({
+  database: env.database, 
+  username: env.username, 
+  password: env.password,
   host: env.host,
+  port: env.port,
   dialect: env.dialect,
-  operatorsAliases: false,
- 
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
+  },
   pool: {
     max: env.max,
     min: env.pool.min,
